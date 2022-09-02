833d4b8f24de10599f447eeb9ea70770ebc2e760
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-09-02 21:00:41 +0300

    small styling correction

--- src/util/styles.js
+++ src/util/styles.js
@@ -8,6 +8,10 @@ export class Style {
     this.styles.push(this.stylesObj[className]);
   }
 
+  remove(className) {
+    this.styles = this.styles.filter((c) => c === className);
+  }
+
   get className() {
     return this.styles.join(" ");
   }
