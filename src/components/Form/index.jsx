c3c70e87e1a40bce98dbedd427ed8ee6a81866b6
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-09-03 19:08:08 +0300

    Form component has an internal submit handler

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,9 +1,17 @@
+import { useFormData } from "../../hooks/form-hook";
 import { FormContext } from "./form-context";
 import styles from "./Form.module.css";
 
 function Form({ id, onSubmit, children }) {
+  const formData = useFormData(id);
+  const formOnSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData.isValid());
+    // onSubmit();
+  };
+  
   return (
-    <form className={styles.form} onSubmit={onSubmit}>
+    <form className={styles.form} onSubmit={formOnSubmit}>
       <FormContext.Provider value={id}>{children}</FormContext.Provider>
     </form>
   );
