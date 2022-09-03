04e3f64898de56761ca84b61f1132c1a78a9bf66
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-09-03 07:49:47 +0300

    modify function to handle invalid dates

--- src/util/date.js
+++ src/util/date.js
@@ -1,6 +1,12 @@
 import { lz } from "./leadingZeros";
 
 export const dateToInput = (date) => {
+  const dt = new Date(date);
+
+  if (dt.toString() === "Invalid Date") {
+    return dt.toString();
+  }
+
   const y = lz(date.getFullYear(), 4);
   const m = lz(date.getMonth() + 1);
   const d = lz(date.getDate());
