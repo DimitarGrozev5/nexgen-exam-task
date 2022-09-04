8a9ff6641d7bdac1f207fc6ba0c7a32f80c1cccc
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-09-04 13:54:49 +0300

    цоммент

--- src/components/FormGroup/index.jsx
+++ src/components/FormGroup/index.jsx
@@ -1,6 +1,7 @@
 import styles from "./FormGroup.module.css";
 
 function FormGroup({ onSubmit, children }) {
+  // TODO: Add group title
   return <div className={styles.group} onSubmit={onSubmit}>{children}</div>;
 }
 
