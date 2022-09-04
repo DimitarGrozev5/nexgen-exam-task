695077b74cec6c8b317068264647e21d3f67e6e7
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-09-04 17:04:21 +0300

    Comment

--- src/components/FormGroup/index.jsx
+++ src/components/FormGroup/index.jsx
@@ -1,7 +1,11 @@
 import styles from "./FormGroup.module.css";
 
+/**
+ * FormGroup organizes it's child components in a single Card,
+ * that provides default styling
+ */
 function FormGroup({ onSubmit, children }) {
-  // TODO: Add group title
+  // TODO: Maybe add group title?
   return <div className={styles.group} onSubmit={onSubmit}>{children}</div>;
 }
 
