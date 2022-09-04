aff69449030cd736cbe9b0fd065875d1f91e4cf5
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-09-04 17:02:59 +0300

    Comments

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -5,8 +5,8 @@ import styles from "./Form.module.css";
 /**
  * Form Component is used to wrap Input Components in an html form
  * The Component also wraps it's child elements in a Form Context, this
- * way the child components know they are in a Form and can get controlled
- * by the Form
+ * way the child components know they are in a Form and can allow the
+ * form to control them
  */
 
 function Form({ id, onSubmit, children }) {
@@ -14,18 +14,18 @@ function Form({ id, onSubmit, children }) {
   const formData = useFormData(id);
 
   // Submit handler checks if all fields are valid and if so, trgiggers the passed onSubmit function
-  const formOnSubmit = (event) => {
+  const submitHandler = (event) => {
     event.preventDefault();
-    console.log(formData.isValid());
-    console.log(formData.getVals());
+    // console.log(formData.isValid());
+    // console.log(formData.getVals());
 
-    if (formData.isValid() && onSubmit) {
+    if (onSubmit && formData.isValid()) {
       onSubmit(formData.getVals());
     }
   };
 
   return (
-    <form className={styles.form} onSubmit={formOnSubmit}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <FormContext.Provider value={id}>{children}</FormContext.Provider>
     </form>
   );
