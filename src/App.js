0a749746363f705d16cd47dd47ab3cf20d6b3971
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-09-05 17:36:31 +0300

    comment

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
   // Setup a handler for the form submit event
   const submitHandler = async (formRawData) => {
 
-    // The handler creates a new FormData object, because the Form contains a file input
+    // The handler creates a new FormData object, because the Form contains a file input, that can't be passed through json
     const formData = new FormData();
     for (const name in formRawData) {
       formData.append(name, formRawData[name]);
